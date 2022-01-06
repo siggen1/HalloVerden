9a71bb618dcf0a753cbf3751695477e485c40051
siggen1 <89570178+siggen1@users.noreply.github.com>
2022-01-06 11:08:16 +0100

    Initial commit

---
+++ src/main/java/com/example/halloverden/HalloVerdenApplication.java
@@ -0,0 +1,13 @@
+package com.example.halloverden;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HalloVerdenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HalloVerdenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/halloverden/HalloVerdenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.halloverden;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HalloVerdenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
